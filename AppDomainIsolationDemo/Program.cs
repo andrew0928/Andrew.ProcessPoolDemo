f88f8186330d4ed4cbc3c30eb5001031c8739604
Andrew Wu <chicken@chicken-house.net>
2020-02-20 04:58:49 +0800

    draft release 3 add appdomain demo - static fields isolation

---
+++ AppDomainIsolationDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using AppDomainIsolationLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainIsolationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomainProgram.InitCount = 543;   // 模擬汙染 static fields 的狀況
+            AppDomainProgram.Main(null);        // 不透過獨立的 AppDomain 執行 code
+
+            var iso = AppDomain.CreateDomain("demo");
+            iso.ExecuteAssemblyByName(typeof(AppDomainProgram).Assembly.FullName);        
+        }
+    }
+
+}
---
+++ AppDomainIsolationLib/AppDomainProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AppDomainIsolationLib
+{
+    public class AppDomainProgram
+    {
+        public static int InitCount = 0;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Init Count: {InitCount} (CurrentDomain: {AppDomain.CurrentDomain.FriendlyName})");
+        }
+    }
+}
